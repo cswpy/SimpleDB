1ee541051745407a581cb0231150beafae117691
cswpy <hncswpy@gmail.com>
2022-03-29 21:09:42 +0400

    Implemented IntegerAggregator operators

--- Lab2/Lab2-starter-code/src/java/simpledb/IntegerAggregator.java
+++ Lab2/Lab2-starter-code/src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,19 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+    private int gbField;
+    private Type gbFieldType;
+    private int agField;
+    private Op aggOp;
+    private HashMap<Field, Integer> gb2val;
+    private HashMap<Field, Integer[]> gb2sum_cnt;
 
     /**
      * Aggregate constructor
@@ -24,6 +32,12 @@ public class IntegerAggregator implements Aggregator {
 
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+    	this.gbField = gbfield;
+    	this.gbFieldType = gbfieldtype;
+    	this.agField = afield;
+    	this.aggOp = what;
+    	this.gb2val = new HashMap<>();
+    	this.gb2sum_cnt = new HashMap<>();
     }
 
     /**
@@ -35,6 +49,54 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+    	Field agfield = tup.getField(agField);
+    	if (agfield.getType() != Type.INT_TYPE) {
+    		throw new IllegalArgumentException("Cannot aggregate on non-integer value");
+    	}
+    	int agval = ((IntField)agfield).getValue();
+    	Field gbfield = null;
+    	if(gbField != Aggregator.NO_GROUPING) {
+    		gbfield = tup.getField(gbField);
+    	}
+    	int newAggVal;
+    	if (aggOp != Op.AVG) {
+    		if(gb2val.containsKey(gbfield)) {
+    			newAggVal = mergeAggregateVal(gb2val.get(gbfield), aggOp, agval);
+    		}else {
+    			if (aggOp == Op.COUNT) {
+    				newAggVal = 1;
+    			}else {
+    				newAggVal = agval;
+    			}
+    		}
+    	}else {
+    		int sum=agval;
+    		int cnt=1;
+    		if(gb2val.containsKey(gbfield)) {
+    			Integer[] oldArr = gb2sum_cnt.get(gbfield);
+    			sum += oldArr[0];
+    			cnt += oldArr[1];
+    		}
+    		gb2sum_cnt.put(gbfield, new Integer[]{sum, cnt});
+    		newAggVal = sum / cnt;
+    	}
+    	gb2val.put(gbfield, newAggVal);
+    	
+    }
+    
+    private int mergeAggregateVal(int oldAggVal, Op aggOp, int newAggVal) {
+    	switch(aggOp) {
+    		case MAX:
+    			return Math.max(oldAggVal, newAggVal);
+    		case MIN:
+    			return Math.min(oldAggVal, newAggVal);
+    		case SUM:
+    			return oldAggVal+newAggVal;
+    		case COUNT:
+    			return oldAggVal+1;
+    		default:
+    			throw new IllegalArgumentException("Illegal agggregation operator");
+    	}
     }
 
     /**
@@ -47,8 +109,27 @@ public class IntegerAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+    	Type[] new_types;
+    	if (gbField == IntegerAggregator.NO_GROUPING) {
+    		new_types = new Type[] {Type.INT_TYPE};
+    	}else {
+    		new_types = new Type[] {gbFieldType, Type.INT_TYPE};
+    	}
+    	TupleDesc newTd = new TupleDesc(new_types);
+    	ArrayList<Tuple> tups = new ArrayList<Tuple>();
+    	
+    	for(Map.Entry<Field, Integer> set : gb2val.entrySet()) {
+    		Tuple tup = new Tuple(newTd);
+    		if (gbField == IntegerAggregator.NO_GROUPING) {
+    			tup.setField(0, new IntField(set.getValue()));
+    		}else {
+    			tup.setField(0, set.getKey());
+    			tup.setField(1, new IntField(set.getValue()));
+    		}
+    		tups.add(tup);
+    	}
+    	
+        return new TupleIterator(newTd, tups);
     }
 
 }
